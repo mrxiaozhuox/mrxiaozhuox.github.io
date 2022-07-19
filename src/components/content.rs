1d00ebfe30d142a0195b1f09a25f7ce5011803b3
YuKun Liu <mrxzx.info@gmail.com>
2022-07-20 01:41:40 +0800

    Initial commit

---
+++ src/components/content.rs
@@ -0,0 +1,32 @@
+use dioxus::prelude::*;
+
+use crate::hooks::markdown::use_markdown;
+
+#[inline_props]
+pub fn Href<'a>(cx: Scope, to: &'a str, children: Element<'a>) -> Element {
+    cx.render(rsx! {
+        a {
+            class: "text-cyan-700 dark:text-cyan-100 underline",
+            href: "{to}",
+            target: "_blank",
+            children
+        }
+    })
+}
+
+#[derive(Props, PartialEq)]
+pub struct MarkdownProps {
+    content: String,
+}
+
+pub fn Markdown(cx: Scope<MarkdownProps>) -> Element {
+    let md_parser = use_markdown(&cx);
+    let content = md_parser(cx.props.content.clone());
+    let _ = js_sys::eval("setTimeout(() => {initMarkdownBody();}, 100);");
+    cx.render(rsx! {
+        div {
+            id: "markdown-body",
+            dangerous_inner_html: "{content}",
+        }
+    })
+}
---
+++ src/pages/starter.rs
@@ -0,0 +1,133 @@
+use std::sync::Arc;
+
+use dioxus::{prelude::*, router::RouterCore};
+use dioxus_toast::ToastInfo;
+
+use crate::{
+    components::{
+        content::{Href, Markdown},
+        footer::Footer,
+    },
+    TOAST_MANAGER,
+};
+
+pub fn HelloDioxus(cx: Scope) -> Element {
+    let window = web_sys::window().unwrap();
+    let input_name = use_state(&cx, String::new);
+    let svc = cx.use_hook(|_| cx.consume_context::<Arc<RouterCore>>());
+    let toast = use_atom_ref(&cx, TOAST_MANAGER);
+    cx.render(rsx! {
+        section {
+            class: "h-screen bg-cover bg-white dark:bg-gray-600",
+            div {
+                class: "flex h-full w-full items-center justify-center container mx-auto px-8",
+                div {
+                    class: "max-w-2xl text-center",
+                    h1 {
+                        class: "text-3xl sm:text-5xl capitalize tracking-widest text-gray-600 dark:text-white lg:text-6xl",
+                        "Dioxus Starter"
+                    }
+                    p {
+                        class: "mt-6 lg:text-lg text-gray-600 dark:text-white",
+                        "Opinionated Dioxus Starter Template"
+                    }
+                    div {
+                        class: "mt-8 flex flex-col space-y-3 sm:-mx-2 sm:flex-row sm:justify-center sm:space-y-0",
+                        input {
+                            r#type: "text",
+                            class: "rounded-md border border-transparent bg-gray-800 dark:bg-white/20 px-4 py-2 text-white placeholder-white backdrop-blur-sm focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 sm:mx-2",
+                            placeholder: "What's your name ?",
+                            oninput: move |e| input_name.set(e.value.clone()),
+                            value: "{input_name}"
+                        }
+                        button {
+                            class: "transform rounded-md bg-blue-700 px-8 py-2 text-sm font-medium capitalize tracking-wide text-white transition-colors duration-200 hover:bg-blue-600 focus:bg-blue-600 focus:outline-none sm:mx-2",
+                            onclick: move |_| {
+                                let name = input_name.get();
+                                if !name.is_empty() {
+                                    if let Some(service) = svc {
+                                        service.push_route(&format!("/hi/{}", name), None, None);
+                                    }
+                                } else {
+                                    toast.write().popup(ToastInfo::warning("Empty input box", "Dioxus Toast"));
+                                }
+                            },
+                            "Go"
+                        }
+                    }
+                    Footer {}
+                }
+            }
+        }
+    })
+}
+
+pub fn SayHi(cx: Scope) -> Element {
+    let route = use_route(&cx);
+    let name = route.segment("name").unwrap();
+    let name = urlencoding::decode(name).expect("UTF-8").to_string();
+    cx.render(rsx! {
+        section {
+            class: "h-screen bg-cover bg-white dark:bg-gray-600",
+            div {
+                class: "flex h-full w-full items-center justify-center container mx-auto px-8",
+                div {
+                    class: "max-w-2xl text-center",
+                    h1 {
+                        class: "text-3xl sm:text-5xl capitalize tracking-widest dark:text-white lg:text-6xl",
+                        "Hi, \"{name}\""
+                    }
+                    Footer {}
+                }
+            }
+        }
+    })
+}
+
+pub fn About(cx: Scope) -> Element {
+    cx.render(rsx! {
+        section {
+            class: "h-screen bg-cover bg-white dark:bg-gray-600",
+            div {
+                class: "flex h-full w-full items-center justify-center container mx-auto px-8",
+                div {
+                    class: "max-w-2xl text-center",
+                    h1 {
+                        class: "text-3xl capitalize tracking-widest dark:text-white",
+                        "About Dioxus Starter"
+                    }
+                    div {
+                        class: "text-gray-800 dark:text-white mt-6",
+                        Href {
+                            to: "/",
+                            "Dioxus Starter"
+                        }
+                        " is a "
+                        Href {
+                            to: "https://dioxuslabs.com/",
+                            "Dioxus Web"
+                        }
+                        " template made by "
+                        Href {
+                            to: "http://github.com/mrxiaozhuox",
+                            "@mrxiaozhuox"
+                        }
+                        " for quick app development"
+                    }
+                    Footer {}
+                }
+            }
+        }
+    })
+}
+
+pub fn MarkdownDisplay(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            class: "pt-6 container mx-auto",
+            Markdown {
+                content: "# Hello World".to_string()
+            }
+        }
+    })
+}
